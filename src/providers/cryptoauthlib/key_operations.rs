bd1de1c4cc5d3caa624227d8e9dd4f1566998b0b
artur.kazimierski <artur.kazimierski@globallogic.com>
2021-02-15 16:27:57 +0100

    initial gen key implementation

--- src/providers/cryptoauthlib/key_operations.rs
+++ src/providers/cryptoauthlib/key_operations.rs
@@ -12,7 +12,25 @@ impl Provider {
         _app_name: ApplicationName,
         _op: psa_generate_key::Operation,
     ) -> Result<psa_generate_key::Result> {
-        Ok(psa_generate_key::Result {})
+        // create key triple
+        let key_attributes = op.attributes;
+        let key_name = op.key_name;
+        let key_triple = KeyTriple::new(app_name, ProviderID::CryptoAuthLib, key_name);
+        // find suitable key id
+        let key_id = 0; // placeholder
+        // generate key
+        // ATCA_STATUS atcab_genkey(uint16_t key_id, uint8_t *public_key)
+        match self.device.genkey(key_id, &public_key) {
+            rust_cryptoauthlib::AtcaStatus::AtcaSuccess => {
+                // update atca_slots
+                Ok(psa_generate_key::Result {})
+            }
+            _ => {
+                let error = ResponseStatus::PsaErrorGenericError;
+                format_error!("Key generation failed ", err);
+                Err(error)
+            }
+        }
     }
 
     pub(super) fn psa_destroy_key_internal(
@@ -20,6 +38,11 @@ impl Provider {
         _app_name: ApplicationName,
         _op: psa_destroy_key::Operation,
     ) -> Result<psa_destroy_key::Result> {
+        // create key triple
+        // get key_id
+        // destroy key
+        // update storage
+
         Ok(psa_destroy_key::Result {})
     }
 }
