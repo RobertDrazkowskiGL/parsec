ae29653ff174508ebe6d9a55484d8fde059a3137
artur.kazimierski <artur.kazimierski@globallogic.com>
2021-03-11 15:29:04 +0100

    rust-calib 0.2.1 adaptation

--- src/providers/cryptoauthlib/asym_sign.rs
+++ src/providers/cryptoauthlib/asym_sign.rs
@@ -11,9 +11,8 @@ impl Provider {
         app_name: ApplicationName,
         op: psa_sign_hash::Operation,
     ) -> Result<psa_sign_hash::Result> {
-        let key_name = op.key_name;
-        let hash = op.hash;
-        let alg = op.alg;
+        let key_name = op.key_name.clone();
+        let hash = op.hash.to_vec();
         let key_triple = self.key_info_store.get_key_triple(app_name, key_name);
         let key_info = match self.get_key_info(&key_triple) {
             Ok(key_info) => key_info,
@@ -24,16 +23,17 @@ impl Provider {
             Err(error) => return Err(error),
         }
 
-        let mut signature = vec![0u8; 64]; // use const
-        match self.device.sign_hash(key_info.id, alg, &hash, &mut signature) {
-            Ok(size) => {
-                signature.resize(size, 0);
-                Ok(psa_sign_hash::Result {
-                    signature: signature.into(),
-                })
-            }
-            Err(error) => {
-                let error = ResponseStatus::from(error);
+        let mut signature = vec![0u8; rust_cryptoauthlib::ATCA_SIG_SIZE];
+        match self.device.sign_hash(
+            rust_cryptoauthlib::SignMode::External(hash),
+            key_info.id[0],
+            &mut signature,
+        ) {
+            rust_cryptoauthlib::AtcaStatus::AtcaSuccess => Ok(psa_sign_hash::Result {
+                signature: signature.into(),
+            }),
+            _ => {
+                let error = ResponseStatus::PsaErrorNotPermitted;
                 format_error!("Sign status: ", error);
                 Err(error)
             }
