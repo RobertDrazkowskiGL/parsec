14777eebd74818108386084d15b258d88e336b35
artur.kazimierski <artur.kazimierski@globallogic.com>
2021-04-12 16:11:46 +0200

    psa_sign_hash, psa_verify_hash implementation

--- src/providers/cryptoauthlib/asym_sign.rs
+++ src/providers/cryptoauthlib/asym_sign.rs
@@ -3,7 +3,7 @@
 use super::Provider;
 use crate::authenticators::ApplicationName;
 use crate::key_info_managers::KeyTriple;
-use parsec_interface::operations::psa_sign_hash;
+use parsec_interface::operations::{psa_sign_hash, psa_verify_hash};
 use parsec_interface::requests::{ProviderID, ResponseStatus, Result};
 
 impl Provider {
@@ -32,6 +32,45 @@ impl Provider {
                 let error = ResponseStatus::PsaErrorNotPermitted;
                 format_error!("Sign status: ", error);
                 Err(error)
+            }
+        }
+    }
+
+    pub(super) fn psa_verify_hash_internal(
+        &self,
+        app_name: ApplicationName,
+        op: psa_verify_hash::Operation,
+    ) -> Result<psa_verify_hash::Result> {
+        let key_name = op.key_name.clone();
+        let hash = op.hash.clone();
+        let signature = op.signature.clone();
+        let key_triple = self.key_info_store.get_key_triple(app_name, key_name);
+        let key_info = match self.get_key_info(&key_triple) {
+            Ok(key_info) => key_info,
+            Err(error) => return Err(error),
+        };
+        match op.validate(key_info.attributes) {
+            Ok(()) => (),
+            Err(error) => return Err(error),
+        }
+
+        // pub fn verify_hash(&self, mode: VerifyMode, hash: &[u8], signature: &[u8]) -> Result<bool, AtcaStatus>
+        match self.device.verify_hash(
+            rust_cryptoauthlib::VerifyMode::Internal(key_info.id[0]),
+            &hash,
+            &signature,
+        ) {
+            Ok(is_verified) => {
+                if !is_verified {
+                    let error = ResponseStatus::PsaErrorInvalidSignature;
+                    format_error!("Verify status: ", error);
+                    return Err(error);
+                }
+                Ok(psa_verify_hash::Result {})
+            }
+            Err(status) => {
+                format_error!("Verify status: ", status);
+                Err(ResponseStatus::PsaErrorGenericError)
             }
         }
     }
--- src/providers/cryptoauthlib/mod.rs
+++ src/providers/cryptoauthlib/mod.rs
@@ -20,7 +20,7 @@ use uuid::Uuid;
 
 use parsec_interface::operations::{
     psa_destroy_key, psa_generate_key, psa_generate_random, psa_hash_compare, psa_hash_compute,
-    psa_sign_hash,
+    psa_sign_hash, psa_verify_hash,
 };
 
 mod generate_random;
@@ -29,6 +29,14 @@ mod key_management;
 mod key_operations;
 mod key_slot;
 mod key_slot_storage;
+
+const SUPPORTED_OPCODES: [Opcode; 5] = [
+    Opcode::PsaGenerateKey,
+    Opcode::PsaDestroyKey,
+    Opcode::PsaHashCompute,
+    Opcode::PsaHashCompare,
+    Opcode::PsaGenerateRandom,
+];
 
 /// CryptoAuthLib provider structure
 #[derive(Derivative)]
@@ -304,6 +312,15 @@ impl Provide for Provider {
     ) -> Result<psa_sign_hash::Result> {
         trace!("psa_sign_hash ingress");
         self.psa_sign_hash_internal(app_name, op)
+    }
+
+    fn psa_verify_hash(
+        &self,
+        app_name: ApplicationName,
+        op: psa_verify_hash::Operation,
+    ) -> Result<psa_verify_hash::Result> {
+        trace!("psa_verify_hash ingress");
+        self.psa_verify_hash_internal(app_name, op)
     }
 }
 
