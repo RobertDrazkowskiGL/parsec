61a659a1ff854e722c90b57e8cd91e233fe4ba1c
artur.kazimierski <artur.kazimierski@globallogic.com>
2021-04-12 15:41:13 +0200

    psa_sign_hash implementation

---
+++ src/providers/cryptoauthlib/asym_sign.rs
@@ -0,0 +1,38 @@
+// Copyright 2021 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+use super::Provider;
+use crate::authenticators::ApplicationName;
+use crate::key_info_managers::KeyTriple;
+use parsec_interface::operations::psa_sign_hash;
+use parsec_interface::requests::{ProviderID, ResponseStatus, Result};
+
+impl Provider {
+    pub(super) fn psa_sign_hash_internal(
+        &self,
+        app_name: ApplicationName,
+        op: psa_sign_hash::Operation,
+    ) -> Result<psa_sign_hash::Result> {
+        let key_triple = KeyTriple::new(app_name, ProviderID::CryptoAuthLib, op.key_name.clone());
+        let key_id = self.key_info_store.get_key_id::<u8>(&key_triple)?;
+        let key_attributes = self.key_info_store.get_key_attributes(&key_triple)?;
+
+        op.validate(key_attributes)?;
+
+        let mut signature = vec![0u8; rust_cryptoauthlib::ATCA_SIG_SIZE];
+        let hash = op.hash.to_vec();
+        match self.device.sign_hash(
+            rust_cryptoauthlib::SignMode::External(hash),
+            key_id,
+            &mut signature,
+        ) {
+            rust_cryptoauthlib::AtcaStatus::AtcaSuccess => Ok(psa_sign_hash::Result {
+                signature: signature.into(),
+            }),
+            _ => {
+                let error = ResponseStatus::PsaErrorNotPermitted;
+                format_error!("Sign status: ", error);
+                Err(error)
+            }
+        }
+    }
+}
