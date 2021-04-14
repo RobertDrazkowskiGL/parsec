9d853b4921f8bbf42b272c3bfdffb329a4515d63
artur.kazimierski <artur.kazimierski@globallogic.com>
2021-04-14 13:51:06 +0200

    psa_export_public_key implementation

--- src/providers/cryptoauthlib/mod.rs
+++ src/providers/cryptoauthlib/mod.rs
@@ -20,6 +20,7 @@ use uuid::Uuid;
 
 use parsec_interface::operations::{
     psa_destroy_key, psa_generate_key, psa_generate_random, psa_hash_compare, psa_hash_compute,
+    psa_export_public_key,
 };
 
 mod generate_random;
@@ -274,6 +275,15 @@ impl Provide for Provider {
     ) -> Result<psa_destroy_key::Result> {
         trace!("psa_destroy_key ingress");
         self.psa_destroy_key_internal(app_name, op)
+    }
+
+    fn psa_export_public_key(
+        &self,
+        app_name: ApplicationName,
+        op: psa_export_public_key::Operation,
+    ) -> Result<psa_export_public_key::Result> {
+        trace!("psa_export_public_key ingress");
+        self.psa_export_public_key_internal(app_name, op)
     }
 }
 
